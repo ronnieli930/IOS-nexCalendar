e090be4807c6c17b11dc27b280345d9ec473a53e
Lee Ronnie <icatchaflyinmyhand@gmail.com>
2019-09-17 18:20:16 +0800

    init empty xcode project

---
+++ NexCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NexCalendar
+//
+//  Created by Ronnie Li on 9/17/19.
+//  Copyright © 2019 Ronnie Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
